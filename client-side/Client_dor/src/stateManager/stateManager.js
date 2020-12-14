fc4849076d21b801074421fb5859d1858bf88213
דור שרעבי <dor29494@gmail.com>
2020-12-14 15:04:51 +0200

    grid 01

--- client-side/Client_dor/src/stateManager/stateManager.js
+++ client-side/Client_dor/src/stateManager/stateManager.js
@@ -1,29 +1,29 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const BsContext = React.createContext();
 const { Provider } = BsContext;
 
-const StateManager = ({children}) => {
-const [first_state, set_first_state] = useState('hello')
-const [ships_array, set_ships_array] = useState([{}])
-const [grid_array, set_grid_array] = useState([[],[],[],[],[],[],[],[],[],[]])
-    const state = {
-      first_state,
-      ships_array,
-      grid_array
-    }
-    const action = {
-      set_first_state,
-      set_ships_array,
-      set_grid_array
-    }
-  
+const StateManager = ({ children }) => {
+  const [first_state, set_first_state] = useState('hello')
+  const [ships_array, set_ships_array] = useState([{}])
+  const [grid_array, set_grid_array] = useState([[], ['A'], ['B'], ['C'], ['D'], ['E'], ['F'], ['G'], ['H'], ['I'], ['J'], [1], [], [], [], [], [], [], [], [], [], [], [2], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []])
+  const state = {
+    first_state,
+    ships_array,
+    grid_array
+  }
+  const action = {
+    set_first_state,
+    set_ships_array,
+    set_grid_array
+  }
 
-    return(
-        <Provider value= {{...state,...action}}>
-        {children}
-        </Provider>
-    )
+
+  return (
+    <Provider value={{ ...state, ...action }}>
+      {children}
+    </Provider>
+  )
 }
 
-export {BsContext ,StateManager}
+export { BsContext, StateManager }
--- client-side/Client_dor/src/view/UserGrid.jsx
+++ client-side/Client_dor/src/view/UserGrid.jsx
@@ -1,23 +1,24 @@
 import React, { useContext } from "react"
 import styled from "styled-components"
-import {BsContext} from "../stateManager/stateManager"
-const UserGrid = ()=>{
-const {ships_array,set_ships_array,grid_array,set_grid_array} = useContext(BsContext)
+import { BsContext } from "../stateManager/stateManager"
+let disabled_area = [];
 
+const UserGrid = () => {
+    const { ships_array, set_ships_array, grid_array, set_grid_array,grid_clicks,setgrid_clicks } = useContext(BsContext)
 
-
-
+    const show = (e) => {
+        if (!grid_clicks[e.target.id]) {
+            console.log(e.target.id);
+            grid_clicks[e.target.id] = e.target.id;
+        }
+    }
 
     return (
-<Wrapper>here
-    <Grid>
-{grid_array.map((grid,index) => 
-    <Pixel>{index}</Pixel>)}
-    </Grid>
-    </Wrapper>
-
-
-
+        <Wrapper>here
+            <Grid>
+                {grid_array.map((grid, index) => <Pixel id={grid[0] || index} key={`g${index}`} onClick={show}>{grid[0] || index}</Pixel>)}
+            </Grid>
+        </Wrapper>
     )
 }
 export default UserGrid
@@ -30,8 +31,20 @@ padding-right: auto;
 const Pixel = styled.div`
 min-width: 2rem;
 min-height: 2rem;
+width: 30px;
+height: 30px;
 border: 2px solid black;
+display: flex;
+align-items: center;
+justify-content: center;
+
+
 `
-const Grid = styled.ul`
+const Grid = styled.div`
+display: flex;
+flex-wrap: wrap;
+height: 330px;
+width: 330px;
+background: yellow;
 
 `
