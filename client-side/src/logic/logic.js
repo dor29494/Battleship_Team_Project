89ab10eb33726ddfa15218b63968ad310139fb1e
Guy Gurfein <guy.gurfein.3@gmail.com>
2021-01-20 20:18:01 +0200

    shorten update_board_hit by 80+used  array methods

--- client-side/src/logic/logic.js
+++ client-side/src/logic/logic.js
@@ -6,18 +6,18 @@ const random_boolean = () => Math.random() < 0.5;
 const update_board_square_around_sink = (board, x, y) => {
     // console.log('trying to update around sink',x,y)
     const new_board = [...board];
-    if(x >= 0 && y >= 0 && x <=9 && y <=9 ){//!= undefined
+    if (x >= 0 && y >= 0 && x <= 9 && y <= 9) {//!= undefined
         if (new_board[x][y].value !== MISS && new_board[x][y].value !== SHIP_PART) { //we want to keep miss visual, also not to change a sunken ship to a aroung sink bc of how loop works
             new_board[x][y].value = AROUND_SINK;
-        }        
+        }
     }
     return new_board;
 };
 
 const update_board_square_around_ship = (board, x, y) => {
     const new_board = [...board];
-    console.log(x,y);
-    if(x >= 0 && y >= 0 && x <=9 && y <=9 ){//!= undefined
+    console.log(x, y);
+    if (x >= 0 && y >= 0 && x <= 9 && y <= 9) {//!= undefined
         if ('around_ship' in new_board[x][y] && new_board[x][y].value !== SHIP_PART) { //making sure we aren't changing the value of a ship part to be an around ship bc of how loop works
             console.log('got in');
             new_board[x][y].around_ship = true;
@@ -28,7 +28,7 @@ const update_board_square_around_ship = (board, x, y) => {
 
 const update_board_around_a_ship = (board, ship, new_value) => {
     let new_board = [...board];
-    const updater = ( x, y) => {
+    const updater = (x, y) => {
         if (new_value === AROUND_SINK) {
             new_board = update_board_square_around_sink(board, x, y);
         }
@@ -39,107 +39,38 @@ const update_board_around_a_ship = (board, ship, new_value) => {
     // updater(new_value, x, y)
     const startX = ship.ship_parts[0].x - 1;
     const startY = ship.ship_parts[0].y - 1;
-    const endX = ship.ship_parts[ship.length-1].x + 1;
-    const endY = ship.ship_parts[ship.length-1].y + 1;
-    for (let i = startX ; i <= endX; i++) {
+    const endX = ship.ship_parts[ship.length - 1].x + 1;
+    const endY = ship.ship_parts[ship.length - 1].y + 1;
+    for (let i = startX; i <= endX; i++) {
         for (let j = startY; j <= endY; j++) {
-            updater(i,j);            
-        }        
+            updater(i, j);
+        }
     }
     return new_board
 }
 
 
-export const update_board_hit = (x, y, ship_index, board, ships,sounds_status) => {
+export const update_board_hit = (x, y, ship_index, board, ships, sounds_status) => {
     let sunk = false;
     let new_ships = [...ships];
     let new_board = [...board];
     const part_index = new_ships[ship_index].ship_parts.findIndex((part) => part.x === x && part.y === y);
     new_ships[ship_index].ship_parts[part_index].is_hit = true;
     new_board[x][y].is_hit = true;
-    
-    let is_ship_sunk = true;
-    for (const ship_part of new_ships[ship_index].ship_parts) {
-        if (!ship_part.is_hit)
-        is_ship_sunk = false;
-    }
+
+    let is_ship_sunk = new_ships[ship_index].ship_parts.every((ship_part) => ship_part.is_hit);
     
     if (is_ship_sunk) {
         new_ships[ship_index].is_sunk = is_ship_sunk;
         new_board = update_board_around_a_ship(new_board, new_ships[ship_index], AROUND_SINK);
     }
-    let is_win = true;
-    new_ships.forEach((ship) => {
-        if (!ship.is_sunk) {
-            is_win = false;
-        }
-    })
-    // for (const ship of new_ships) {
-    //     if (!ship.is_sunk){
-    //         is_win = false;
-
-    // }
-    // }
-
-    // console.log('About to enter switch case with x&y',x,y)
-    switch (x) {
-        case 0:
-            switch (y) {
-                case 0:
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y + 1));
-                    break;
-                case 9:
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y - 1));
-                    break;
-
-                default:
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y + 1));
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y - 1));
-                    break;
-            }
-            break;
-
-        case 9:
-            switch (y) {
-                case 0:
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y + 1));
-
-                    break;
-                case 9:
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y - 1));
-
-                    break;
-
-                default:
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y + 1));
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y - 1));
-                    break;
-            }
-            break;
-
-        default:
-            switch (y) {
-                case 0:
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y + 1));
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y + 1));
-                    break;
-                case 9:
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y - 1));
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y - 1));
-                    break;
-
-                default:
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y + 1));
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y + 1));
-                    new_board = update_board_square_around_sink(new_board, (x + 1), (y - 1));
-                    new_board = update_board_square_around_sink(new_board, (x - 1), (y - 1));
-                    break;
-            }
-
-            break;
-    }
-    // console.log('Updated around_sink:')
-
+    let is_win = new_ships.every((ship) => ship.is_sunk);
+    
+    new_board = update_board_square_around_sink(new_board, (x + 1), (y + 1));
+    new_board = update_board_square_around_sink(new_board, (x - 1), (y + 1));
+    new_board = update_board_square_around_sink(new_board, (x + 1), (y - 1));
+    new_board = update_board_square_around_sink(new_board, (x - 1), (y - 1));
+    
     if (is_win)
         // if (false)
         // win the game
@@ -252,9 +183,7 @@ export const initial_game_board = (board = [[], [], [], [], [], [], [], [], [], 
                 value: SEA,
                 around_ship: false
             })
-
         }
-
     }
     return new_board;
 }
